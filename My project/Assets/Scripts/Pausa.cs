24df8821053db2fb3e43042b26fd46a15ba3785c
Marcos <gomarmarcos@gmail.com>
2024-10-14 22:19:13 -0300

    Pausa y gravedad

---
+++ My project/Assets/Scripts/Pausa.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+            {
+                Resumir();
+            }
+            else
+            {
+                Pausa();
+            }
+        }
+    }
+
+    public void Pausa()
+    {
+        pausePanel.SetActive(true); 
+        Time.timeScale = 0f;
+        isPaused = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Resumir()
+    {
+        pausePanel.SetActive(false); 
+        Time.timeScale = 1f;      
+        isPaused = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void VolverAlMenu()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("El juego se ha cerrado."); 
+    }
+}
+
