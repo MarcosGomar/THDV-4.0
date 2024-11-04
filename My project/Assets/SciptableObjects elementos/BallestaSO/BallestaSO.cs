1bcf6ed95b9ff9c87b0f7115211937f9ade3dbb4
Marcos <gomarmarcos@gmail.com>
2024-11-04 09:51:56 -0300

    Un error terrible pasó, no se como pasó
    Tube que reemplazarlo con la ultima versión en el git

---
+++ My project/Assets/SciptableObjects elementos/BallestaSO/BallestaSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[CreateAssetMenu]
+public class BallestaSO : ScriptableObject
+{
+    public float distanciaParaSubir = 1f;
+    public float tiempoParaSubir = 1f;
+    public float velocidadDeSubida = 2f;
+    public float velocidadDeBajada = 2f;
+    public float tiempoDeEsperaParaRegresar = 10f;
+    public int vidaInicialTorreEnemiga = 100;
+    public string textoInteraccion = "Mantén E para interactuar y disparar";
+    public string textoVidaTorre = "Torre Enemiga: ";
+}
