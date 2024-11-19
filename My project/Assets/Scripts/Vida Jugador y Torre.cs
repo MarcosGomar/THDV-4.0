992db53b42fd869bdd36d615cd70648363a3531d
Marcos <gomarmarcos@gmail.com>
2024-11-19 19:52:51 -0300

    Arreglo de balance, daño enemigo y eliminación por ataque y no colisión

--- My project/Assets/Scripts/Vida Jugador y Torre.cs
+++ My project/Assets/Scripts/Vida Jugador y Torre.cs
@@ -9,6 +9,7 @@ public class VidaJugadoryTorre : MonoBehaviour
     public ValoresVidaSO vidaEntidad; 
     public TextMeshProUGUI healthText;
     private int currentHealth;
+    private float collisionTime = 0f;
 
     void Start()
     {
@@ -54,7 +55,29 @@ public class VidaJugadoryTorre : MonoBehaviour
     {
         if (collision.gameObject.tag == "Enemigo")
         {
-            Daño(vidaEntidad.damageAmount); 
+            Daño(vidaEntidad.damageAmount);
+            collisionTime = 0f;
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemigo")
+        {
+            collisionTime += Time.deltaTime;
+            if (collisionTime >= 6f)
+            {
+                Daño(vidaEntidad.damageAmount);
+                collisionTime = 0f;
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemigo")
+        {
+            collisionTime = 0f;
         }
     }
 }
